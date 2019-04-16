dd3144b30d8fd46d4571777e9a3173149f005b95
Guo Duhao <i@nyan.im>
2019-04-16 10:25:15 +0800

    Update PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -4,10 +4,7 @@ class Solution:
         stack = []
         for i in range(0,len(s)//2):
             stack.append(s[i])
-        if len(s)%2 == 1:
-            s = s[len(s)//2+1:]
-        else:
-            s = s[len(s)//2:]
+        s = s[len(s)//2+len(s)%2:]
         for i in s:
             if i != stack[-1]:
                 return False

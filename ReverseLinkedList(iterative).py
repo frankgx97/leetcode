a33f4d35aa0326d6780823b63b9abb5f68f2094b
Guo Duhao <i@nyan.im>
2019-05-30 19:19:55 +0800

    Update and rename ReverseLinkedList(TLE).py to ReverseLinkedList(iterative).py

--- ReverseLinkedList(iterative).py
+++ ReverseLinkedList(iterative).py
@@ -3,7 +3,6 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-import copy
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
@@ -11,7 +10,7 @@ class Solution:
         current = head
         next = None
         while current != None:
-            next = copy.deepcopy(current.next)
+            next = current.next
             current.next = tail
             tail = current
             current = next

82bae955bc9b9629766da7f498f024e7d52e239a
Guo Duhao <i@nyan.im>
2019-06-18 23:11:56 +0800

    Update 3Sum(dfs-tle).py

--- 3Sum(dfs-tle).py
+++ 3Sum(dfs-tle).py
@@ -4,13 +4,12 @@ class Solution:
             if len(path) > 3:
                 return
             if len(path) == 3 and sum(path) == target:
-                if sorted(path) not in duplicate:
-                    duplicate.append(sorted(path))
+                path.sort()
+                if path not in result:
                     result.append(path)
                 return
             for i in range(index, len(nums)):
                 dfs(path+[nums[i]], target, i+1)
-        duplicate = []
         result = []
         dfs([], 0, 0)
         return result

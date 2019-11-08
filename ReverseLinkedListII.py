f7dfe05f961c57023d9a42fdb9bd61cfa02be89c
Guo Duhao <i@nyan.im>
2019-11-08 16:35:32 -0500

    Update ReverseLinkedListII.py

--- ReverseLinkedListII.py
+++ ReverseLinkedListII.py
@@ -6,41 +6,47 @@
 
 class Solution:
     def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-            
+        '''
+        manipulate pointers - ac
+        use a helper function to reverse list of selected range, returns the head and tail node after reversion
+        then iterate the list, find the node that is before the reverse head, after the reverse tail, and start the reverse.
+        then connect the nodes together
+        use dummy head to handle modification to head node
+        '''
+        def rev(h,c):
+            i=0
+            prev = None
+            current = h
+            nex = None
+            while i<=c:
+                nex = current.next
+                current.next = prev
+                prev = current
+                current = nex
+                i+=1
+            return prev,h #head and tail after reversion
+                
         dummy = ListNode(-1)
         dummy.next = head
         current = dummy
-        
-        if m == n:
-            return dummy.next
         
         i=0
         current = dummy
         prev = dummy
         
+        before_head = None
+        after_tail = None
+        start_rev = None
+        
         while current:
             if i == m:
-                start = current
-                left = prev
+                before_head = prev
+                start_rev = current
             if i == n:
-                end = current
-                right = current.next
-            current = current.next
-            if i>=1:
-                prev = prev.next     
-
-            i += 1
-            
-        current = start.next
-        prev = start
-        end.next = None
-        while current:
-            temp = current.next
-            current.next = prev
+                after_tail = current.next
+            i+=1
             prev = current
-            current = temp
-
-        left.next = end
-        start.next = right
-            
+            current = current.next
+        before_head.next, tail = rev(start_rev, n-m)
+        tail.next = after_tail
         return dummy.next

118d418620fde685840c4ac25db6093c9995d9a5
Guo Duhao <i@nyan.im>
2019-11-09 01:04:29 -0500

    Update Searcha2DMatrix.py

--- Searcha2DMatrix.py
+++ Searcha2DMatrix.py
@@ -1,28 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def binarySearch(array, target):
-            if array == []:
-                return (0,False)
-            left = 0
-            right = len(array)-1
-            mid = 0
-            while(left <= right):
-                mid = (right - left)//2 + left
-                if array[mid] < target:
-                    left = mid + 1
-                elif array[mid] > target:
-                    right = mid - 1
-                else:
-                    return (mid,True)
-            return (right, False)
-
-        if matrix == []:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
             return False
-        lst = []
+        def bs(l,target):
+            left = 0
+            right = len(l)-1
+            while left <= right:
+                mid = (left + right) //2
+                if l[mid] == target:
+                    return mid  
+                elif l[mid] > target:
+                    right = mid -1
+                else:
+                    left = mid + 1
+            return right
+        
+        first_col = []
         for i in matrix:
-            if i == []:
-                break
-            else:
-                lst.append(i[0])
-        vertical = binarySearch(lst, target)[0]
-        return binarySearch(matrix[vertical], target)[1]
+            first_col.append(i[0])
+        row_no = bs(first_col,target)
+        row = matrix[row_no]
+        index = bs(row,target)
+        if row[index] == target:
+            return True
+        else:
+            return False

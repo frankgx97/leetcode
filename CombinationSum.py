cb6265577e93bc1fff20b6b33afc78ad514081df
Guo Duhao <i@nyan.im>
2019-11-15 16:50:54 -0500

    Update CombinationSum.py

--- CombinationSum.py
+++ CombinationSum.py
@@ -1,26 +1,21 @@
 class Solution:
     '''
-    https://www.tamarous.com/leetcode-backtrace-problems/
+    dfs - ac
+    all candidates are positive, so backtrack if path sum reaches beyond target
+    ref: https://www.tamarous.com/leetcode-backtrace-problems/
     '''
-    
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        candidates.sort()
-        self.dfs(candidates, target, 0, [], res,0)
-        return res
-
-    def dfs(self, nums, target, index, path, res, depth):
-        depth+=1
-        depthstr = ''
-        for i in range(0,depth):
-            depthstr += '-'
-        #print(depthstr,nums, target, index, path, res)
-        if target < 0:
-            #print(depthstr, 'target:', target, '<0','backtrack!')
-            return  # backtracking
-        if target == 0:
-            res.append(path)
-            #print(depthstr, 'solution found:', path)
-            return 
-        for i in range(index, len(nums)):
-            self.dfs(nums, target-nums[i], i, path+[nums[i]], res, depth)
+        r = []
+        def dfs(path):
+            if sum(path) == target:
+                path.sort()
+                if path not in r:
+                    r.append(path)
+                return
+            if not candidates or sum(path) > target:
+                return
+            for i in candidates:
+                if sum(path+[i]) <= target:
+                    dfs(path+[i])
+        dfs([])
+        return r

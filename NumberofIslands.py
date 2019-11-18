fee3d5a3126fbd874bdf9d0b1133f96547445301
Guo Duhao <i@nyan.im>
2019-11-17 20:30:19 -0500

    Update and rename NumberofIslands(floodfill).py to NumberofIslands.py

---
+++ NumberofIslands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        '''
+        dfs - flood fill
+        '''
+        def dfs(i,j):
+            grid[i][j] = '0'
+            if i+1 < m and grid[i+1][j] == '1':
+                dfs(i+1,j)
+            if i-1 >= 0 and grid[i-1][j] == '1':
+                dfs(i-1,j)
+            if j+1 < n and grid[i][j+1] == '1':
+                dfs(i,j+1)
+            if j-1 >= 0 and grid[i][j-1] == '1':
+                dfs(i,j-1)
+            return
+        
+        if len(grid) == 0 or len(grid[0]) == 0:
+            return 0
+        m = len(grid)
+        n = len(grid[0])
+        c = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    dfs(i,j)
+                    c += 1
+        return c

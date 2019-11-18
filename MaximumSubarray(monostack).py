e87f8f820968cd68e6e406f3d229c7fa83cbfcce
Guo Duhao <i@nyan.im>
2019-11-17 21:37:48 -0500

    Update MaximumSubarray(monostack).py

--- MaximumSubarray(monostack).py
+++ MaximumSubarray(monostack).py
@@ -1,16 +1,23 @@
+import math
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        r = max(nums)
-        for i in range(len(nums)):
-            if i > 0:
-                nums[i] += nums[i-1]
-        stack = [0]
-        for i in range(len(nums)):
-            if not stack or stack[-1] < nums[i]:
-                stack.append(nums[i])
-                r = max(r, nums[i] - stack[0])
+        '''
+        mono stack
+        '''
+        m = max(nums) #to handle mono-decrease input
+        for i in range(1,len(nums)):
+            nums[i] = nums[i] + nums[i-1]
+        stack = [0] #to handle mono-increase input
+        
+        
+        for i in nums:
+            if not stack or i > stack[-1]:
+                stack.append(i)
+                m = max(m,stack[-1] - stack[0])
             else:
-                while(stack and stack[-1] > nums[i]):
-                    stack.pop()
-                stack.append(nums[i])
-        return r
+                curr = 0
+                while stack and i <= stack[-1]:
+                    curr = i - stack.pop(-1)
+                stack.append(i)
+                
+        return m

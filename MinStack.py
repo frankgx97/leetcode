b61cfc84020b0f0b61c949cdaa8888458b347616
Guo Duhao <i@nyan.im>
2019-11-19 21:33:29 -0500

    Update MinStack.py

--- MinStack.py
+++ MinStack.py
@@ -1,25 +1,47 @@
+import heapq
 class MinStack:
-    data = list()
+    '''
+    heap - ac
+    use heap to store the min
+    when poping, check if the heap root is the same with popped item, if yes, pop the heap.
+    '''
 
     def __init__(self):
         """
         initialize your data structure here.
         """
+        self.heap = []
         self.data = []
+        
 
     def push(self, x: int) -> None:
         self.data.append(x)
+        heapq.heappush(self.heap,x)
         return
+        
 
     def pop(self) -> None:
-        self.data.pop()
+        if self.data:
+            x = self.data.pop(-1)
+            if self.heap[0] == x:
+                heapq.heappop(self.heap)
         return
+        
+        
 
     def top(self) -> int:
-        return self.data[-1]
+        if self.data:
+            return self.data[-1]
+        else:
+            return -1
         
+
     def getMin(self) -> int:
-        return min(self.data)
+        if self.heap:
+            return self.heap[0]
+        else:
+            return -1
+        
 
 
 # Your MinStack object will be instantiated and called as such:

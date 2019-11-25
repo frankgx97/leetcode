90822bc66a1bca2115d6bb7ea69acb487bcd77b4
Guo Duhao <i@nyan.im>
2019-11-24 22:24:45 -0500

    Create NestedListWeightSum(bfs).py

---
+++ NestedListWeightSum(bfs).py
@@ -0,0 +1,62 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        r = 0
+        queue = copy.deepcopy(nestedList)
+        level = 1
+        while queue:
+            #print(queue)
+            curr = copy.deepcopy(queue)
+            queue = []
+            for i in range(len(curr)):
+                if curr[i].isInteger():
+                    r += curr[i].getInteger() * level
+                else:
+                    for j in curr[i].getList():
+                        queue.append(j)
+            curr = []
+                
+            level+=1
+        return r

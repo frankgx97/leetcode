b8074f6ac59d0d6850c8170306400b9476e2df6b
Guo Duhao <i@nyan.im>
2019-12-01 17:13:06 -0500

    Update RemoveDuplicatesfromSortedArray.py

--- RemoveDuplicatesfromSortedArray.py
+++ RemoveDuplicatesfromSortedArray.py
@@ -1,5 +1,27 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        '''
+        use index to access the array - ac
+        each time check if arr[index+1] is equal to arr[index]
+        if yes, remove item of index+1,
+        else index++
+        
+        O(n) time and constant space
+        '''
+        p=0
+        while p < len(nums):
+            if p + 1 < len(nums):
+                if nums[p+1] == nums[p]:
+                    nums.pop(p+1)
+                else:
+                    p += 1
+            else:
+                break
+        return len(nums)
+        
+        '''
+        O(n) time and space solution
+        '''
         dup = []
         for i in nums[:]:
             if i not in dup:

95cc9ee80d1edf9c09a620b29db1b1dd609a5730
Guo Duhao <i@nyan.im>
2019-12-01 22:21:56 -0500

    Update NextGreaterElementII.py

--- NextGreaterElementII.py
+++ NextGreaterElementII.py
@@ -6,29 +6,21 @@ class Solution:
         to improve performance, we can break the loop once reach the max item in the array in the extra part.
         in mono stack and dict, store the array index instead of the value to handle the case that duplicated entries exist in the array.
         '''
+        original_length = len(nums)
+        nums = nums+nums
         stack = []
         dic = {}
-        numlength = len(nums)
-        nums = nums+nums
-        max_in_array = 0
-        
         for i in range(len(nums)):
-            max_in_array = max(max_in_array,nums[i])
-            if not stack or nums[stack[-1]] > nums[i]:
+            if not stack or nums[i] <= nums[stack[-1]]:
                 stack.append(i)
             else:
-                while stack and nums[stack[-1]] < nums[i]:
-                    if stack[-1] not in dic:
-                        dic[stack.pop(-1)] = nums[i]
-                    else:
-                        stack.pop(-1)
+                while stack and nums[i] > nums[stack[-1]]:
+                    dic[stack.pop()] = nums[i]
                 stack.append(i)
-            if i > numlength-1 and nums[i] == max_in_array:
-                break
         while stack:
-            dic[stack.pop(-1)] = -1
-
+            dic[stack.pop()] = -1
         r = []
-        for i in range(numlength):
+        for i in range(original_length):
             r.append(dic[i])
         return r
+                

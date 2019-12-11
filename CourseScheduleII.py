6e9556d4664ec37a0f024cb5c8588a0a5b0d5198
Guo Duhao <i@nyan.im>
2019-12-11 00:36:59 -0500

    Create CourseScheduleII.py

---
+++ CourseScheduleII.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        '''
+        DAG bfs topology sort - ac
+        ref: alien dictionary
+        '''
+        graph = {}
+        ind = [0]*numCourses
+        for i in range(numCourses):
+            graph[i] = []
+        for i in prerequisites:
+            graph[i[1]].append(i[0])
+            ind[i[0]] += 1
+        
+        # bfs
+        r = []
+        queue = []
+        for i in range(len(ind)):
+            if ind[i] == 0:
+                r.append(i)
+                queue.append(i)
+        while queue:
+            curr = queue.pop(0)
+            for i in graph[curr]:
+                ind[i] -= 1
+                if ind[i] == 0:
+                    queue.append(i)
+                    r.append(i)
+        if len(r) != numCourses:
+            return []
+        else:
+            return r

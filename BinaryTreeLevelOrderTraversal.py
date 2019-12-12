75587509d5f2dcb0a28cb374cdcfd49f52b72290
Guo Duhao <i@nyan.im>
2019-12-11 20:40:13 -0500

    Update BinaryTreeLevelOrderTraversal.py

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -7,27 +7,26 @@
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        '''
+        bfs with queue
+        '''
         def bfs(root):
-            q = [root]
-            counter = len(q)
-            temp = []
-            result = []
-            direction = True
-            while len(q) > 0:
-                curr = q[0]
-                q.pop(0)
-                if curr:
-                    temp.append(curr.val)
-                    q.append(curr.left)
-                    q.append(curr.right)
-
-                counter -= 1
-                if counter == 0:
-                    direction = not direction
-                    counter = len(q)
-                    if len(temp) > 0:
-                        result.append(temp)
-                    temp = []
-            return result
-                    
+            if not root:
+                return []
+            queue = [root]
+            r = []
+            while queue:
+                innerqueue = []
+                for i in queue:
+                    innerqueue.append(i)
+                queue = []
+                curr = []
+                for i in innerqueue:
+                    curr.append(i.val)
+                    if i.left:
+                        queue.append(i.left)
+                    if i.right:
+                        queue.append(i.right)
+                r.append(curr)
+            return r
         return bfs(root)

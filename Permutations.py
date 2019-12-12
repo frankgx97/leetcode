536481bb6227efc5eb56e91591fd8fc16e453967
Guo Duhao <i@nyan.im>
2019-12-12 15:22:49 -0500

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -1,20 +1,18 @@
 class Solution:
-    candidates = []
-    results = []
     def permute(self, nums: List[int]) -> List[List[int]]:
-        self.results = []
-        self.candidates = nums
-        self.dfs([],[])
-        return self.results
-        
-    def dfs(self, index, path):
-        if len(path) == len(self.candidates):
-            self.results.append(path)
-            return
-        elif len(path) > len(self.candidates):
-            return
-        else:
-            for i in range(0, len(self.candidates)):
+        '''
+        dfs - ac
+        ref: https://www.youtube.com/watch?v=oCGMwvKUQ_I
+        '''
+        result = []
+        def dfs(index, path):
+            if len(path) == len(nums):
+                result.append(path)
+                return
+            for i in range(len(nums)):
                 if i in index:
                     continue
-                self.dfs(index+[i], path+[self.candidates[i]])
+                dfs(index+[i],path+[nums[i]])
+            return
+        dfs([],[])
+        return result

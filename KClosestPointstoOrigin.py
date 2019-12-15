c33adf8ea3c10b75ce110455d911470d80afb84a
Guo Duhao <i@nyan.im>
2019-12-15 15:07:21 -0500

    Update KClosestPointstoOrigin.py

--- KClosestPointstoOrigin.py
+++ KClosestPointstoOrigin.py
@@ -1,15 +1,17 @@
+import heapq
+import math
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        dic = {}
-        for i in range(len(points)):
-            distance = abs(points[i][0])*abs(points[i][0]) + abs(points[i][1])*abs(points[i][1])
-            if distance not in dic.keys():
-                dic[distance] = [i]
-            else:
-                dic[distance].append(i)
-        l = sorted(list(dic.keys()))
+        '''
+        heap - ac
+        time O(nlogn)
+        space O(n)
+        '''
+        heap = []
+        for i in points:
+            curr_distance = math.sqrt(i[0]**2+i[1]**2)
+            heapq.heappush(heap,(curr_distance,i))
         r = []
-        for i in range(0,min(K,len(l))):
-            for j in dic[l[i]]:
-                r.append(points[j])
-        return r[:K]
+        for i in range(K):
+            r.append(heapq.heappop(heap)[1])
+        return r

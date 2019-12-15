70aabeb6813449f73ab52ef065f30866ae510aab
Guo Duhao <i@nyan.im>
2019-12-15 00:38:18 -0500

    Update TwoSum(Hashmap).py

--- TwoSum(Hashmap).py
+++ TwoSum(Hashmap).py
@@ -1,10 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i, vi in enumerate(nums):
-            if (target - vi) in hashmap.keys():
-                if hashmap[target - vi] != i:
-                    return [hashmap[target - vi],i]
-            else:
-                hashmap[vi] = i
-            
+        '''
+        time: O(n)
+        space: O(n)
+        '''
+        dic = {}
+        for i in range(len(nums)):
+            dic[nums[i]] = i
+
+        for i in range(len(nums)):
+            if target - nums[i] in dic and dic[target - nums[i]] != i:
+                return [dic[target - nums[i]],i]

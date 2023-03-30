1eb71c2b43e3b5d4ac1ca468e3d2399d03089f35
Guo Duhao <dguo@guoduhao.me>
2023-03-29 22:40:06 -0400

    Update CourseSchedule.py

--- CourseSchedule.py
+++ CourseSchedule.py
@@ -1,69 +1,27 @@
+'''
+topological sorting
+'''
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        '''
-        DAG bfs topology sort - ac
-        ref: alien dictionary
-        '''
-        graph = {}
-        indegree = [0]*numCourses
-        
-        for i in range(numCourses):
-            graph[i] = []
-        
-        for c in prerequisites:
-            j,i = tuple(c)
-            graph[i].append(j)
-            indegree[j] += 1
+        graph = [[] for _ in range(numCourses)]
+        ind = [0] * numCourses
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            ind[course] += 1
         
         q = []
-        
-        visited = set()
-        
-        count = numCourses
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
-                q.append(i)
-                visited.add(i)
-                count -= 1
-        
-        while q:
-            subq = q[:]
-            q = []
-            for i in subq:
-                for j in graph[i]:
-                    if indegree[j] >= 1:
-                        indegree[j] -= 1
-                    if indegree[j] == 0 and j not in visited:
-                        q.append(j)
-                        visited.add(j)
-                        count -= 1
-        return count == 0
-            
-        
-        #==============================================
-        graph = {}
-        ind = [0]*numCourses
-        for i in range(numCourses):
-            graph[i] = []
-        for i in prerequisites:
-            graph[i[1]].append(i[0])
-            ind[i[0]] += 1
-        
-        # bfs
-        r = []
-        queue = []
+        taken = set()
+
         for i in range(len(ind)):
             if ind[i] == 0:
-                r.append(i)
-                queue.append(i)
-        while queue:
-            curr = queue.pop(0)
-            for i in graph[curr]:
-                ind[i] -= 1
-                if ind[i] == 0:
-                    queue.append(i)
-                    r.append(i)
-        if len(r) != numCourses:
-            return False
-        else:
-            return True
+                q.append(i)
+                taken.add(i)
+        while q:
+            curr = q.pop(0)
+            for succ in graph[curr]:
+                ind[succ] -= 1
+                if ind[succ] == 0:
+                    q.append(succ)
+                    taken.add(succ)
+        return len(taken) == numCourses

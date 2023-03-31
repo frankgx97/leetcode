9ac198fbb04f9686ebc2f573aac131ec4cb7cc5b
Guo Duhao <dguo@guoduhao.me>
2023-03-30 23:20:48 -0400

    Update ImplementTrie.py

--- ImplementTrie.py
+++ ImplementTrie.py
@@ -1,63 +1,36 @@
-class TrieNode:
-    def __init__(self,v):
-        self.v = v
-        self.is_end = 0
-        self.nodes = [None]*26
-
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.root = TrieNode('')
+        self.trie = {}
         
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        current = self.root
-        for i in range(len(word)):
-            if current.nodes[ord(word[i]) - ord('a')] == None:
-                new_node = TrieNode(word[i])
-                current.nodes[ord(word[i]) - ord('a')] = new_node
-                current = new_node
-            else:
-                current = current.nodes[ord(word[i]) - ord('a')]
-                continue
-        current.is_end = 1
-        return
-                
+        curr = self.trie
+        for i in word:
+            if i not in curr:
+                curr[i] = {}
+            curr = curr[i]    
+        curr['*'] = '*'
         
-
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-        current = self.root
-        for i in range(len(word)):
-            if current.nodes[ord(word[i]) - ord('a')] == None:
+        curr = self.trie
+        for i in word:
+            if i not in curr:
                 return False
             else:
-                current = current.nodes[ord(word[i]) - ord('a')]
-        if current.is_end:
-            return True
-        else:
-            return False
+                curr = curr[i]
+        return '*' in curr
         
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        current = self.root
-        for i in range(len(prefix)):
-            if current.nodes[ord(prefix[i]) - ord('a')] == None:
+        curr = self.trie
+        for i in prefix:
+            if i not in curr:
                 return False
             else:
-                current = current.nodes[ord(prefix[i]) - ord('a')]
+                curr = curr[i]
         return True
+        
 
 
 # Your Trie object will be instantiated and called as such:
@@ -65,5 +38,3 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
-
-[1,2,3,4,4]
